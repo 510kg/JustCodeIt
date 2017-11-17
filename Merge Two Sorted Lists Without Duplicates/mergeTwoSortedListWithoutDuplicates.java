9bfdd7950f0e51365dffa444c537e6382fb22162
Song <kensongxy@gmail.com>
2017-11-16 17:05:33 -0800

    Update
    for merge sorted lists without duplicates

--- Merge Two Sorted Lists Without Duplicates/mergeTwoSortedListWithoutDuplicates.java
+++ Merge Two Sorted Lists Without Duplicates/mergeTwoSortedListWithoutDuplicates.java
@@ -8,8 +8,10 @@ class ListNode {
 	}
 }
 
-public class Solution {
-	public static ListNode mergeTwoListsWihoutDuplicates(ListNode l1, ListNode l2) {
+class Solution {
+    Solution() {}
+
+	public ListNode mergeTwoListsWihoutDuplicates(ListNode l1, ListNode l2) {
 		if (l1 == null && l2 == null) {
 			return null;
 		}
@@ -50,5 +52,32 @@ public class Solution {
 		newHead.next = null;
 		
 		return dummy.next;
+	}
+
+	public ListNode converter(int[] vals) {
+		ListNode origin = new ListNode(vals[0]);
+		ListNode temp = origin;
+		for (int i = 1; i < vals.length; i++) {
+			temp.next = new ListNode(vals[i]);
+			temp = temp.next;
+		}
+		return origin;
+	}
+
+    public static void main(String args[]) {
+    	Solution solution = new Solution();
+
+    	int[] vals1 = new int[] {1, 2, 2, 3, 3, 5};
+    	int[] vals2 = new int[] {3, 3, 4, 5, 6, 6, 6};
+		
+		ListNode n1 = solution.converter(vals1);
+		ListNode n2 = solution.converter(vals2);
+		
+		ListNode head = solution.mergeTwoListsWihoutDuplicates(n1, n2);
+		
+		while (head != null) {
+			System.out.println(head.val);
+			head = head.next;
+		}
 	}
 }
