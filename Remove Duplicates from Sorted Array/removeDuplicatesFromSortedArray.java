a909138b3437bf86ec2ef1b773be63abf61b2bd8
Song <kensongxy@gmail.com>
2017-11-17 16:29:51 -0800

    update
    for lc 26 most voted aws

--- Remove Duplicates from Sorted Array/removeDuplicatesFromSortedArray.java
+++ Remove Duplicates from Sorted Array/removeDuplicatesFromSortedArray.java
@@ -1,7 +1,8 @@
 class Solution {
 	Solution() {}
-
-    public int removeDuplicates(int[] nums) {
+    
+    //My Solution
+    public int removeDuplicates1(int[] nums) {
     	if (nums == null || nums.length == 0) {
     		return 0;
     	}
@@ -16,6 +17,22 @@ class Solution {
     	return slow;
     }
 
+    //Most Voted
+    public int removeDuplicates2(int nums) {
+    	if (nums == null || nums.length == 0) {
+    		return 0;
+    	}
+    	if (nums.length < 2) {
+    		return nums.length;
+    	}
+    	int count = 1;
+    	for (int i = 1; i < nums.length; i++) {
+    		if (nums[i] != nums[i - 1]) {
+    			nums[count++] = [nums[i];
+    		}
+    	}
+    }
+
 	public static void main(String[] args) {
         Solution solution = new Solution();
 
@@ -23,9 +40,9 @@ class Solution {
         int[] nums2 = new int[] {0, 0, 0, 0, 0, 0};
         int[] nums3 = new int[] {1, 2, 3, 4, 5, 6};
 
-        int count1 = solution.removeDuplicates(nums1);
-        int count2 = solution.removeDuplicates(nums2);
-        int count3 = solution.removeDuplicates(nums3);
+        int count1 = solution.removeDuplicates1(nums1);
+        int count2 = solution.removeDuplicates1(nums2);
+        int count3 = solution.removeDuplicates1(nums3);
 
         System.out.println(count1);
         System.out.println(count2);
