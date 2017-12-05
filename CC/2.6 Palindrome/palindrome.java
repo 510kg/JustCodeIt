1d21c4e593d8ae6a61393dd0614cb42ddd507fe5
Song <kensongxy@gmail.com>
2017-12-05 15:48:33 -0800

    update
    for cc 2.6

--- CC/2.6 Palindrome/palindrome.java
+++ CC/2.6 Palindrome/palindrome.java
@@ -11,18 +11,60 @@ class Solution {
 		}
 	}
 
-	
+	public boolean palindromeList(ListNode head) {
+		if (head == null) {
+			return false;
+		}
+		/*
+		if (head.next == null) {
+			return true;
+		}
+		*/
+		ListNode fast = head;
+		ListNode slow = head;
+		while (fast.next != null && fast.next.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+
+		ListNode newHead = slow.next;
+		ListNode prev = null;
+		while (newHead != null) {
+			ListNode temp = newHead.next;
+			newHead.next = prev;
+			prev = newHead;
+			newHead = temp;
+		}
+
+		while (prev != null) {
+			if (prev.val != head.val) {
+				return false;
+			}
+			head = head.next;
+			prev = prev.next;
+		}
+
+		return true;
+	}
 
 	private ListNode converter(int[] vals) {
 		ListNode head = new ListNode(vals[0]);
-		ListNoe curr = head;
+		ListNode curr = head;
 		for (int i = 1; i < vals.length; i++) {
 			curr.next = new ListNode(vals[i]);
 			curr = curr.next;
 		}
+		return head;
 	} 
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
+
+		int[] vals = new int[] {1, 2, 3, 4, 3, 2, 1};
+		ListNode head = solution.converter(vals);
+
+		boolean result = solution.palindromeList(head);
+
+		System.out.println(result);
 	}
 }
