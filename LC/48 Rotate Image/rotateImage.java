c5b7f9fe398c72381c702b4f08fc5a69f0391dae
Song <kensongxy@gmail.com>
2017-12-06 16:44:24 -0800

    update
    for lc 48

--- LC/48 Rotate Image/rotateImage.java
+++ LC/48 Rotate Image/rotateImage.java
@@ -1,15 +1,35 @@
 import java.util.*;
 
 class Solution {
-	Solution() {}
+    Solution() {}
 
-	public void rotate(int[][] matrix) {
-        
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix[0] == null) {
+            return ;
+        }
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i ++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
     }
 
-	public static void main(String[] args) {
-		Solution solution = new Solution();
+    public static void main(String[] args) {
+        Solution solution = new Solution();
 
+        int[][] matrix = new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {12, 14, 15, 16}};
+        solution.rotate(matrix);
 
-	}
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(";");
+        }
+    }
 }
