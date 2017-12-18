e5d1eae4e1382c3fd71e478bb661e6547f2f7fbe
Song <kensongxy@gmail.com>
2017-12-18 12:33:51 -0800

    update
    for lc 53

---
+++ LC/53 Maximum Subarray/maximumSubarray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+	Solution() {}
+
+	public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        
+        int max = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            max = Math.max(max, sum);
+            sum = Math.max(sum, 0);
+        }
+
+        return max;
+    }
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+	}
+}
---
+++ LC/73 Set Matrix Zeroes/setMatrixZeroes.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Solution {
+	Solution() {}
+
+	public void zeroMatrix(int[][] matrix) {
+		if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+			return ;
+		}
+		boolean firstRow = true;
+		boolean firstCol = true;
+		for (int i = 0; i < matrix.length; i++) {
+			if (matrix[i][0] == 0) {
+				firstCol = false;
+				break;
+			}
+		}
+		for (int j = 0; j < matrix[0].length; j++) {
+			if (matrix[0][j] == 0) {
+				firstRow = false;
+				break;
+			}
+		}
+
+		for (int i = 1; i < matrix.length; i++) {
+			for (int j = 1; j < matrix[0].length; j++) {
+				if (matrix[i][j] == 0) {
+					matrix[0][j] = 0;
+					matrix[i][0] = 0;
+				}
+			}
+		}
+
+		for (int i = 1; i < matrix.length; i++) {
+			for (int j = 1; j < matrix[0].length; j++) {
+				if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+
+		if (!firstCol) {
+			for (int i = 0; i < matrix.length; i++) {
+				matrix[i][0] = 0;
+			}
+		}
+
+		if (!firstRow) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				matrix[0][j] = 0;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+
+		int[][] matrix1 = new int[][] {{1, 0, 3, 4, 5}, {0, 7, 8, 9, 10}, {11, 12, 13, 0, 15}, {0, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
+		int[][] matrix2 = new int[][] {{0,0,0,5}, {4,3,1,4}, {0,1,1,4}, {1,2,1,3}, {0,0,1,1}};
+		solution.zeroMatrix(matrix1);
+
+		for (int i = 0; i < matrix1.length; i++) {
+			for (int j = 0; j < matrix1[0].length; j++) {
+				System.out.print(matrix1[i][j] + " ");
+			}
+			System.out.println(";");
+		}
+	}
+}
